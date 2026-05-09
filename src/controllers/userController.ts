b3ac330c7f018e5d764bec8fc0305c4348d7af4d
NickoIkhwan <nicko25januari@gmail.com>
2026-05-09 12:48:17 +0700

    feat (db): add neondb, drizzle orm

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,21 +1,40 @@
 import { Request, Response } from "express";
+import { db } from "../DB";
+import { users } from "../DB/schema";
 
-export const getUser = (req : Request, res : Response) =>{
-const users = [
-    {id :1, name : "panji"},
-    {id :2, name : "Davi"}
-];
+export const getUser = async (req: Request, res: Response) => {
 
-res.json({
-    status:"success",
-    data:users
-});
-
+  try {
+    const allUsers = await db.select().from(users);
+    res.json({
+      status: "success",
+      data: allUsers,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Gagal mengambil data user",
+    });
+  }
 };
 
-export const createUser = (req : Request, res : Response) => {
-    res.status(201).json({
-        status:"success",
-        message:`User ${name} berhasil dibuat`
+export const createUser = async (req: Request, res: Response) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({
+      status: "fail",
+      message: "Nama user diperlukan",
     });
-}
+  }
+
+  try {
+    await db.insert(users).values({ name });
+    res.status(201).json({
+      status: "Success",
+      message: `User ${name} berhasil dibuat`,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Gagal membuat user",
+    });
+  }
+};
